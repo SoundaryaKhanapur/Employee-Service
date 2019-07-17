e3a107c5d8a95f73d19c0c982a5d3c357ac31dd8
Soundarya <36791174+SoundaryaKhanapur@users.noreply.github.com>
2019-07-17 22:45:15 +0530

    Update EmployeeServiceApplication.java

--- EmployeeServiceApplication.java
+++ EmployeeServiceApplication.java
@@ -3,8 +3,9 @@ package com.git.company.employeeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import io.micrometer.core.ipc.http.HttpSender.Request;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -37,5 +38,20 @@ public class EmployeeServiceApplication {
                 .description("APIs to manage employee details")
                 .contact("Soundarya Khanapur")
                 .build();
+    }
+    
+    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Employee Not Found")  // 404
+    public static class EmployeeNotFoundException extends RuntimeException {
+        
+    }
+    
+    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Looks like Employee is already deleted or does not exist")  // 404
+    public static class EmployeeAlreadyDeletedException extends RuntimeException {
+        
+    }
+    
+    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Employee does not exist")  // 404
+    public static class EmployeeDoesNotExistException extends RuntimeException {
+        
     }
 }
